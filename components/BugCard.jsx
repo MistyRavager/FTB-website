24480e384c820e735a5d94e3e502c778600b5edc
MistyRavager <kushagra.gupta0103@gmail.com>
2024-03-16 00:44:48 +0530

    first commit by Jaswanth

---
+++ components/BugCard.jsx
@@ -0,0 +1,11 @@
+export default function BugCard() {
+    return (
+     <div className=''>
+      <div className='bg-black/80 w-[20rem] h-[20rem] rounded-lg shadow-md px-2 flex flex-col gap-2 items-center justify-center'>
+        <h2 className='text-4xl bg-white text-black uppercase p-2 rounded-md'>Flutter</h2>
+        <button className='bg-blue-500 underline p-2 rounded-md'>Start Challenge!</button>
+      </div>
+     </div>
+    )
+  }
+  
