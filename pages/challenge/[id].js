dda9b07855a92a7978ab49317635a1862b06c12f
MistyRavager <kushagra.gupta0103@gmail.com>
2024-03-17 04:04:53 +0530

    test prod and make relevant changes

--- pages/challenge/[id].js
+++ pages/challenge/[id].js
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-const index = () => {
+export default function ID() {
     const data = {
         flutter: {
             github: "",
@@ -97,4 +97,3 @@ const index = () => {
     );
 };
 
-export default index;
